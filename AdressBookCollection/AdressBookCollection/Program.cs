420f499bc6b5aabe6adb26ad0a8bbff048d91f8a
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-07 00:06:21 +0530

    [gurpreet] UC12_SortbyStateorCity

--- AdressBookCollection/AdressBookCollection/Program.cs
+++ AdressBookCollection/AdressBookCollection/Program.cs
@@ -39,7 +39,7 @@ namespace CompleteAddressBook
 //7
 //Alphabetically_Sorted_List
 //gagana
-//Gurpreetsingh
+//gurpreetsingh
 //******WELCOME TO ADDRESS BOOK******
 //1.Add_Contact
 //2.Display_Contact
@@ -49,3 +49,47 @@ namespace CompleteAddressBook
 //6.Count_Contacts
 //7.Sort_Contacts
 //8
+//*******************************uc11***********************************//
+//Enter FirstName: gagana
+//Enter LastName: koder
+//Enter Address : hvs
+//Enter City : cta
+//Enter State : mp
+//Enter zip : jwdvc
+//Enter Contact No: whdkvc
+//Enter Email: kwhd
+//Details Added Successfully.
+
+//******WELCOME TO ADDRESS BOOK******
+//1.Add_Contact
+//2.Display_Contact
+//3.Delet_Contact
+//4.Update_Contact
+//5.Serch_FromAllContact
+//6.Count_Contacts
+//7.Sort_Contacts
+//8.Exit
+//Enter Your Choice:
+//7
+//Chooose option TO Sort Contacts by
+//1. FirstName
+//2. City
+//3. State
+//4. zip
+//2
+//Alphabetically_Sorted_List
+//----------------------------------------------------------------------
+//FirstName   LastName   Address, City, State, Zip, Contact, Email
+//----------------------------------------------------------------------
+//Shreegowri        koder           dsgh    Bng     kar    56788   wydhf    wjkdbc
+//gagana    koder           hvs     cta     mp     jwdvc   whdkvc           kwhd
+//******WELCOME TO ADDRESS BOOK******
+//1.Add_Contact
+//2.Display_Contact
+//3.Delet_Contact
+//4.Update_Contact
+//5.Serch_FromAllContact
+//6.Count_Contacts
+//7.Sort_Contacts
+//8.Exit
+//Enter Your Choice:
