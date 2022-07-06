0a2bdaa1ed3cfb7ada2cb254e007b523a62a3261
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-06 23:24:47 +0530

    [gurpreet] UC3_EditContact

---
+++ AdressBook.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23_CollectionAddressBook
+{
+    internal class AddressBook
+    {
+        //Creating Object Of Class
+        Contact tempContact = new Contact();
+        //Creating Dictionary
+        public Dictionary<string, Contact> contacts;
+
+        //Initializing Dictionary
+        public AddressBook()
+        {
+            contacts = new Dictionary<string, Contact>();
+        }
+
+        //Method Used To Create Contacts
+        public void CreateContact()
+        {
+            tempContact.GetUserInfo();
+            string name = tempContact.GetName();
+            if (contacts.ContainsKey(name) is false)
+            {
+                contacts.Add(name, tempContact);
+            }
+            else
+            {
+                Console.WriteLine("erorr");
+            }
+        }
+
+        //Method Used To Add Contacts
+        public void AddContacts()
+        {
+            tempContact.GetUserInfo();
+            string name = tempContact.GetName();
+            if (contacts.ContainsKey(name) is false)
+            {
+                contacts.Add(name, tempContact);
+                Console.WriteLine("Successfully Added A New Contact!!!");
+            }
+            else
+            {
+                Console.WriteLine("erorr");
+            }
+
+        }
+
+        //Method Used To Edit Contacts
+        public void EditContact()
+        {
+            Console.WriteLine("Enter name of contact to edit: ");
+            string name = Console.ReadLine();
+            if (contacts.ContainsKey(name) is true)
+            {
+                Contact tempContact = new Contact();
+                tempContact.GetUserInfo();
+                string editName = tempContact.GetName();
+                if (contacts.ContainsKey(editName) is false || editName == name)
+                {
+                    contacts.Remove(name);
+                    contacts.Add(editName, tempContact);
+                    Console.WriteLine("Successfully Edited And Saved!!!");
+                    Display();
+                }
+                else
+                    Console.WriteLine("Edited name is invalid");
+            }
+            else
+                Console.WriteLine("Name does not exist");
+        }
+
+
+        //Method Used To Display The Contacts
+        public void Display()
+        {
+            foreach (string name in contacts.Keys)
+            {
+                contacts[name].Display();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23_CollectionAddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===================Welcome To Address Book===================");
+            //Object Of Class
+            AddressBook address = new AddressBook();
+
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nEnter Number to Execute the Address book Program \n1. Create contacts \n2. Add contact \n3. Edit contact \n4. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Creating A New Contact");
+                        address.CreateContact();
+                        address.Display();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Adding A New Contact");
+                        address.AddContacts();
+                        address.Display();
+                        Console.WriteLine();
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Editing Existing Contact");
+                        address.EditContact();
+                        Console.WriteLine();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("If You Want To Exit Then Press Enter");
+                        flag = false;
+                        Console.ReadKey();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid");
+                        break;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
